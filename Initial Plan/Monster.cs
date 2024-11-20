47822db0fdebba824cd1221a0ba934e86ac97fa7
idris-k <midris.myusri@gmail.com>
2024-11-21 00:03:50 +0800

    yes

---
+++ Initial Plan/Monster.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swarmgame2
+{
+    public class Monster
+    {
+        public int Health { get; set; }
+        public Vector2 Position { get; set; }
+        public float Speed { get; set; }
+
+        public void Move()
+        {
+            // Logic to move the monster
+            
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health <= 0)
+            {
+                Die();
+            }
+        }
+
+        public void Attack()
+        {
+            // Logic for the monster to attack
+        }
+
+        public void Die()
+        {
+            // Logic to handle monster death
+        }
+    }
+
+
+}
